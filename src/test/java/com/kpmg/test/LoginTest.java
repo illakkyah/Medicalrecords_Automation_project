9d11e9505e148d439eb7c60a905d0053740daad1
illakkyah <illakkyahraveendran@gmail.com>
2026-04-08 23:50:21 +0530

    added data provider

--- src/test/java/com/kpmg/test/LoginTest.java
+++ src/test/java/com/kpmg/test/LoginTest.java
@@ -1,6 +1,7 @@
 package com.kpmg.test;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.kpmg.base.AutomationWrapper;
@@ -8,15 +9,31 @@ import com.microsoft.playwright.options.SelectOption;
 
 public class LoginTest extends AutomationWrapper {
 
-	@Test
-	public void validLoginTest() {
-		page.locator("xpath=//input[@id='authUser']").fill("physician");
-		page.locator("xpath=//input[@id='clearPass']").fill("physician");
-		page.locator("xpath=//select[@name='languageChoice']").selectOption(new SelectOption().setValue("18"));
+	@DataProvider
+	public String[][] loginCredentials() {
+
+		String[][] loginDetails = new String[2][4];
+		loginDetails[0][0] = "admin";
+		loginDetails[0][1] = "pass";
+		loginDetails[0][2] = "18";
+		loginDetails[0][3] = "Calendar";
+		loginDetails[1][0] = "physician";
+		loginDetails[1][1] = "physician";
+		loginDetails[1][2] = "18";
+		loginDetails[1][3] = "Calendar";
+
+		return loginDetails;
+	}
+
+	@Test(dataProvider = "loginCredentials")
+	public void validLoginTest(String userName, String passWord, String langValue, String expectedText) {
+		page.locator("xpath=//input[@id='authUser']").fill(userName);
+		page.locator("xpath=//input[@id='clearPass']").fill(passWord);
+		page.locator("xpath=//select[@name='languageChoice']").selectOption(new SelectOption().setValue(langValue));
 		page.locator("xpath=//button[@id='login-button']").click();
 		String actualtext = page.locator("xpath=//span[text()='Calendar']").innerText();
-		Assert.assertEquals(actualtext, "Calendar");
-		
+		Assert.assertEquals(actualtext, expectedText);
+
 	}
 
 	@Test
@@ -25,7 +42,7 @@ public class LoginTest extends AutomationWrapper {
 		page.locator("xpath=//input[@id='clearPass']").fill("admin123");
 		page.locator("xpath=//select[@name='languageChoice']").selectOption(new SelectOption().setValue("18"));
 		page.locator("xpath=//button[@id='login-button']").click();
-		String errorText=page.locator("xpath=//p[contains(text(),'Invalid username')]").innerText(); 
+		String errorText = page.locator("xpath=//p[contains(text(),'Invalid username')]").innerText();
 		Assert.assertTrue(errorText.contains("Invalid username"));
 
 	}
