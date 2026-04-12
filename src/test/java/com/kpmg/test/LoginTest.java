93af419c4da4aa5a68fdca22a9095c2a255712dd
illakkyah <illakkyahraveendran@gmail.com>
2026-04-12 19:44:48 +0530

    dataprovider added

--- src/test/java/com/kpmg/test/LoginTest.java
+++ src/test/java/com/kpmg/test/LoginTest.java
@@ -5,34 +5,22 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.kpmg.base.AutomationWrapper;
+import com.kpmg.utilities.DataSource;
 import com.microsoft.playwright.options.SelectOption;
 
 public class LoginTest extends AutomationWrapper {
 
-	@DataProvider
-	public String[][] loginCredentials() {
-
-		String[][] loginDetails = new String[2][4];
-		loginDetails[0][0] = "admin";
-		loginDetails[0][1] = "pass";
-		loginDetails[0][2] = "18";
-		loginDetails[0][3] = "Calendar";
-		loginDetails[1][0] = "physician";
-		loginDetails[1][1] = "physician";
-		loginDetails[1][2] = "18";
-		loginDetails[1][3] = "Calendar";
-
-		return loginDetails;
-	}
-
-	@Test(dataProvider = "loginCredentials")
+	@Test(dataProvider = "validLoginData",dataProviderClass = DataSource.class)
 	public void validLoginTest(String userName, String passWord, String langValue, String expectedText) {
 		page.locator("xpath=//input[@id='authUser']").fill(userName);
 		page.locator("xpath=//input[@id='clearPass']").fill(passWord);
 		page.locator("xpath=//select[@name='languageChoice']").selectOption(new SelectOption().setValue(langValue));
 		page.locator("xpath=//button[@id='login-button']").click();
-		String actualtext = page.locator("xpath=//span[text()='Calendar']").innerText();
-		Assert.assertEquals(actualtext, expectedText);
+		/*String actualtext = page.locator("xpath=//span[text()='Calendar']").innerText();
+		Assert.assertEquals(actualtext, expectedText);*/
+		
+		String errorText = page.locator("xpath=//p[contains(text(),'Invalid username')]").innerText();
+		Assert.assertTrue(errorText.contains("Invalid username"));
 
 	}
 
---
+++ src/test/java/com/kpmg/utilities/DataSource.java
@@ -0,0 +1,95 @@
+package com.kpmg.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+
+public class DataSource {
+
+	@DataProvider(name = "validLoginData")
+	public String[][] loginCredentials() throws Exception {
+
+		/*
+		 * String[][] loginDetails = new String[2][4];
+		 * 
+		 * loginDetails[0][0] = "admin"; loginDetails[0][1] = "pass"; loginDetails[0][2]
+		 * = "18"; loginDetails[0][3] = "Calendar";
+		 * 
+		 * loginDetails[1][0] = "physician"; loginDetails[1][1] = "physician";
+		 * loginDetails[1][2] = "18"; loginDetails[1][3] = "Calendar";
+		 * 
+		 * return loginDetails;
+		 */
+
+		FileInputStream file = new FileInputStream("src/test/resources/test-data/Open-EMR-data.xlsx");
+		XSSFWorkbook book = new XSSFWorkbook(file);
+		XSSFSheet sheet = book.getSheet("validLoginTest");
+		/*
+		 * String value = sheet.getRow(1).getCell(0).getStringCellValue();
+		 * System.out.println(value);
+		 */
+
+		int rowlength = sheet.getPhysicalNumberOfRows();
+
+		int columnlength = sheet.getRow(0).getPhysicalNumberOfCells();
+
+		// System.out.println(rowlength);
+		// System.out.println(columnlength);
+
+		String[][] logindata = new String[rowlength - 1][columnlength];
+
+		for (int i = 0; i < rowlength - 1; i++) {
+
+			for (int j = 0; j < columnlength; j++) {
+				DataFormatter type = new DataFormatter();
+				logindata[i][j] = type.formatCellValue(sheet.getRow(i + 1).getCell(j));
+				// String value = sheet.getRow(i).getCell(j).getStringCellValue();
+				// System.out.println(value);
+			}
+
+		}
+
+		book.close();
+		file.close();
+
+		/*
+		 * for(String[] logData:logindata) {
+		 * System.out.println(Arrays.toString(logData)); }
+		 */
+		return logindata;
+	}
+
+	@DataProvider(name = "invalidLoginData")
+	public String[][] invalidLoginCredentials() throws Exception {
+
+		FileInputStream file = new FileInputStream("src/test/resources/test-data/Open-EMR-data.xlsx");
+		XSSFWorkbook book = new XSSFWorkbook(file);
+		XSSFSheet sheet = book.getSheet("invalidLoginTest");
+
+		int rowlength = sheet.getPhysicalNumberOfRows();
+
+		int columnlength = sheet.getRow(0).getPhysicalNumberOfCells();
+
+		String[][] logindata = new String[rowlength - 1][columnlength];
+
+		for (int i = 0; i < rowlength - 1; i++) {
+
+			for (int j = 0; j < columnlength; j++) {
+				DataFormatter type = new DataFormatter();
+				logindata[i][j] = type.formatCellValue(sheet.getRow(i + 1).getCell(j));
+			}
+
+		}
+
+		book.close();
+		file.close();
+
+		return logindata;
+	}
+
+}
