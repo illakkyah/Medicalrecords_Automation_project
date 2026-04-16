e0a929626a35d2438b9c4beef1251227f62958b8
illakkyah <illakkyahraveendran@gmail.com>
2026-04-16 22:48:40 +0530

    added PatientTest

--- src/test/java/com/kpmg/test/PatientTest.java
+++ src/test/java/com/kpmg/test/PatientTest.java
@@ -7,10 +7,11 @@ import org.testng.annotations.Test;
 
 import com.kpmg.base.AutomationWrapper;
 import com.microsoft.playwright.FrameLocator;
+import com.microsoft.playwright.assertions.LocatorAssertions.ContainsTextOptions;
 import com.microsoft.playwright.options.SelectOption;
 
-public class PatientTest extends AutomationWrapper{
-	
+public class PatientTest extends AutomationWrapper {
+
 	@Test
 	public void addValidPatientTest() {
 		page.locator("xpath=//input[@id='authUser']").fill("admin");
@@ -21,13 +22,16 @@ public class PatientTest extends AutomationWrapper{
 		Assert.assertEquals(actualtext, "Calendar");
 
 		// Frame frame=page.frame("logoutinnerframe");
-		page.locator("xpath=//div[text()='OpenEMR Product Registration']").scrollIntoViewIfNeeded();
-		page.locator("xpath=//div[text()='OpenEMR Product Registration']").isVisible();
-		//System.out.println(popup);
+		// page.locator("xpath=//div[text()='OpenEMR Product
+		// Registration']").scrollIntoViewIfNeeded();
+		// page.locator("xpath=//div[text()='OpenEMR Product
+		// Registration']").isVisible();
+		// System.out.println(popup);
 		// page.locator("xpath=//div[text()='OpenEMR Product
 		// Registration']").scrollIntoViewIfNeeded();
 		// page.evaluate("document.body.style.zoom='0.8'");
-		page.locator("xpath=//button[text()='Ask again later']").click();
+
+		// page.locator("xpath=//button[text()='Ask again later']").click();
 		page.locator("xpath=//div[text()='Patient']").hover();
 		page.locator("xpath=//div[text()='New/Search']").click();
 		FrameLocator frame = page.frameLocator("xpath=//iframe[@name='pat']");
@@ -35,13 +39,16 @@ public class PatientTest extends AutomationWrapper{
 
 		frame.locator("xpath=//input[@placeholder='Last Name']").fill("wick");
 
-		frame.locator("xpath=//input[@id='form_DOB']").fill("15-4-2026");
+		frame.locator("xpath=//input[@id='form_DOB']").fill("16-4-2026");
 		frame.locator("xpath=//select[@title='Birth Sex']").selectOption(new SelectOption().setLabel("Female"));
-		frame.locator("xpath=//button[@name='create']").click();
-
-		FrameLocator frame1 =page.frameLocator("xpath=//iframe[@id='modalframe']");
-		frame1.locator("xpath=//button[contains(text(),'Confirm Create ')]").click();
-		
+		frame.locator("xpath=//button[@id='create']").click();
+		page.waitForTimeout(2000);
+		FrameLocator frame1 = page.frameLocator("xpath=//iframe[@id='modalframe']");
+		// frame1.locator("xpath=//button[contains(text(),'Confirm Create ')]").click();
+		frame1.locator("xpath=//button[@id='confirmCreate']").click();
+		String actualDashboardHeader = frame.locator("xpath=//span[contains(text(),'Medical Record Dashboard')]")
+				.innerText();
+		Assert.assertTrue(actualDashboardHeader.contains("Medical Record Dashboard"));
 	}
 
 }
